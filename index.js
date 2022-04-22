87c3dad6a557a9b274229e428e206c3ac153ce87
tahlil <bsse0803@iit.du.ac.bd>
2022-04-22 18:20:17 +0600

    Extract function arguments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const loadjson = require('jsonfile')
+const jsonfile = require('jsonfile')
 const path = require('path');
 const fs = require('fs');
 
@@ -9,7 +9,27 @@ fs.readdir(directoryPath, function (err, files) {
     } 
    
     files.forEach(function (file) {
-        
-        console.log(file); 
+        jsonfile.readFile(path.join('abis', file), function (err, obj) {
+            if (err) console.error(err)
+            try {
+                for (let index = 0; index < obj.length; index++) {
+                    let entity = obj[index];
+                    if(entity.type === "function"){
+                        let arguments = [];
+                        console.log("Function name: " + entity.name);
+                        for(let i = entity.inputs.length; i--;){
+                            arguments.push(entity.inputs[i].type)
+                        }
+                        console.log("Arguments:");
+                        let argumentsString = arguments.join(",")
+                        console.log(argumentsString);
+                    }
+                    
+                    
+                }
+            } catch (error) {
+                console.log(error);
+            }
+          })
     });
 });
